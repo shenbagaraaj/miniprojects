1319dd58f04c56ecb8c1b43ffcc7896b835230f7
shenbagaraaj <135063176+shenbagaraaj@users.noreply.github.com>
2023-05-31 15:41:11 +0530

    Some spots in CVP

---
+++ spotselection.py
@@ -0,0 +1,81 @@
+"""Spot selection in kovilpatti"""
+from PIL import Image
+print("Welcome!")
+print("Here are some major spots in Kovilpatti 1. Kathiresan Temple (Mountain Temple), 2. Shenbagavalli amman temple, 3. Ramasamydas Park, 4. Meltin Cafe, 5. Oyalo Pizza, 6. Krishna Farm milk, 7. Grace resturant, 8. Sunday smiley")
+while True:
+    user_input = input("Please choose your selection or you want to quit type Q ").lower()
+    if user_input == "q":
+        print("Good Bye, Nice to meet you! ")
+        break
+
+    if user_input == "kathiresan temple":
+        print("You're choosing Kathiresan Temple, right")
+        user_confirmation = input("Kindly confirm that Y or N ").lower()
+        if user_confirmation == "y":
+            print("It's an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "shenbagavalli amman temple":
+        print("You're choosing Shenbagavalli amman temple, Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "ramasamy das park":
+        print("You're choosing Ramasamy das Park , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "meltin cafe":
+        print("You're Meltin Cafe , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "oyalo pizza":
+        print("You're choosing Oyalo Pizza , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "krishna farm milk":
+        print("You're choosing Krishna Farm milk  , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "grace resturant":
+        print("You're choosing Grace Resturant , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    if user_input == "sunday smiley":
+        print(" You're choosing Sunday Smiley , Right")
+        user_confirmation = input("Kindly confirm that Y or N  ").lower()
+        if user_confirmation == "y":
+            print("Its an amazing spot. Please find the below attached image it would be helpful for you to reach the location. ")
+        else:
+            print("Choose your selection soon, Basket of fun loaded things waiting for you there. ")
+
+    else:
+        print("You're choosing the wrong one. Kindly choose the correct one. ")
+
+
+
+
+    
